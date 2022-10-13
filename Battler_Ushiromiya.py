ae989c30d79cbcbcd7ca5ae2cd4eb631233287a7
Dmitri Naufal Ghani <dmitri.naufal@gmail.com>
2022-10-13 20:40:59 +0700

    Commit Pertama

---
+++ Battler_Ushiromiya.py
@@ -0,0 +1,15 @@
+from importlib import import_module
+import importlib
+from modulefinder import IMPORT_NAME
+from re import search
+import sys
+
+
+print ("Battler Ushiromiya")
+print ("Golden Longsword")
+
+import_module
+
+def tambah (bil1, bil2):
+    hasil = bil1 + bil2
+    print ("hasil tambah dari", bil1, "+", bil2, "=", hasil)
