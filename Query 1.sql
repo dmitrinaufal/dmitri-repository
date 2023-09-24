46924c8ff6a28be1b3032ff40c3ddc0babd4cbda
dmitrinaufal <73144846+dmitrinaufal@users.noreply.github.com>
2023-09-24 22:42:13 +0700

    Testing

--- Query 1.sql
+++ Query 1.sql
@@ -1,8 +1,1 @@
---  Mengambil Semua Film yang Tersedia Berdasarkan Bahasa (Language)
-select
-    f.title as film_title,
-    l.name as language
-from
-    film as f
-inner join
-    language as l on f.language_id = l.language_id;
+
